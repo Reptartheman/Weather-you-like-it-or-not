52a32fae2771d8c5e240bc65897e28efd8f7fcae
Reptartheman <117610334+Reptartheman@users.noreply.github.com>
2023-01-07 20:57:17 -0500

    addedplaceholderimages

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,47 @@
-const requestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}"
+const apiKey = "8160474e6f23dd64a3ea9a9e05d2989d";
+const requestUrl = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
+const searchButton = document.querySelector(".btn");
+
+
+searchButton.addEventListener("click", function(){
+
+    
+    console.log("working");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* GIVEN a weather dashboard with form inputs
+
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city */
